0a1e8a6e3e80575b13517f94c02a15ec8bd2df3d
Juan Jose Fernandez <juanjoseferna@javerianacali.edu.co>
2024-08-29 15:28:34 -0500

    Presentacion

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 import time
 
 def lambda_handler(event, context):
-    print("Politicas y tratamientos de los datos: mucho texto from github 2 \n - Fecha y hora:", time.strftime("%Y-%m-%d %H:%M:%S"))
+    print("Politicas y tratamientos de los datos: mucho texto from github el dia de la presentacion \n - Fecha y hora:", time.strftime("%Y-%m-%d %H:%M:%S"))
     response = {
         'statusCode': 200,
         'body': json.dumps('Política de tratamiento de datos aceptada')
